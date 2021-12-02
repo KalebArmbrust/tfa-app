d254478303f8a2ad4ec7d7c3f4c242ebfdc5cbc0
Kaleb Armbrust <armbrustcopyco@gmail.com>
2021-12-02 15:38:08 -0800

    boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header class="header">
+        <p class="header-title">TFAwesome</p>
       </header>
+      <div class="form-container">
+        <h2 class="form-title">Guild-Info</h2>
+        <form class="form">
+          <div class="form-dividers">
+            <label class="form-label">Name</label>
+            <input class="form-input"></input>
+          </div>
+          <div class="form-dividers">
+            <label class="form-label">Code</label>
+            <input class="form-input"></input>
+          </div>
+          <button class="form-button">Enter</button>
+        </form>
+      </div>
     </div>
   );
 }
